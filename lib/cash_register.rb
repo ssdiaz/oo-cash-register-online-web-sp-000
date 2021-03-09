ca3f236a37c8ad580b672493fb47f583c70f4b61
Sam D <samidiaz3@gmail.com>
2021-03-09 05:37:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -36,7 +36,7 @@ class CashRegister
   def void_last_transaction
   #  last_item = self.add_item
     #self.total = self.total - last_item
-    @total = @total - last_item
+    self.total = self.total - last_item
   end
 
 end
