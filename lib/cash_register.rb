4bfa77508d74517df795d8bde8de1c367cd38704
Sam D <samidiaz3@gmail.com>
2021-03-09 05:25:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,7 +33,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = self.total - self.total[-1]
+    self.total = (self.total - self.total[-1]).to_f
     # @items.pop
     # if @items == nil
     #     @total.clear
